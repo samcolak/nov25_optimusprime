e5263355ecf78b2b10d241d0649be0c3c22be4ed
Samuel Colak <sam@samcolak.com>
2025-10-21 18:42:30 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+# OptimusPrime - November 2025 Challenge
+
+Determine the highest prime number within 30 seconds
+
+## What is a Prime Number
+
+A prime number is a number that can only be divisable by itself and one.
+
+### Side notes
+
+1. 0 is not a prime number
+2. All even numbers (except 2) are not prime numbers
+3. The maximum number of steps to determine a factor is 1/2 of the number being resolved
+4. Anything divisable by 5 is also not a prime (as it is odd, this is checked by step 2)
+
+## Challenge
+
+Part of the Rust developers challenge
+
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+
+#![allow(while_true)]
+
+
+fn is_prime(n: i32) -> bool {
+
+    match n {
+
+        0 => return false,
+        
+        1 | 2 => return true,
+                
+        k => {
+
+            if k % 2 == 0 {
+                return false;
+            } else if (k % 5 == 0) && (k != 5) {
+                return false
+            } else {
+                let _m = (n-1) / 2;
+                if _m > 1 {
+                    let mut _t = 3;
+                    while _t <= _m {
+                        if n % _t == 0 {
+                            return false;
+                        }
+                        _t += 2;
+                    }
+                }
+            }
+
+        }
+
+    };
+
+    true
+
+}
+
+
+fn main() {
+
+    let mut _n = 1;
+    let mut _inc = 1;
+
+    while true {
+
+        if is_prime(_n) {
+            println!("{_n} is prime");
+        }
+
+        _n += _inc;
+
+        if (_n > 6) && (_inc == 1) {
+            _inc = 2;
+        }
+
+    };
+
+}
